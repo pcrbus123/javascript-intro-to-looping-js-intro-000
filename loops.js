f7867f69bd5c05ff3d7dc3f692c476031ce9e57e
Pnina Breslav <pcrbus123@gmail.com>
2018-11-04 06:03:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
 function forLoop(array){
 for (let i=0; i< 25; i++){
 {return array + `I am ${i} strange loop`}
+}}
 
 
-
-}
 function whileLoop(n){
   while (n > 0){console.log (--n)};
 console.log('done')
