c9e996cd41a31ec9942e0d4941a6ce84181c252f
Pnina Breslav <pcrbus123@gmail.com>
2018-11-04 05:55:37 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+for (var i=0, i< 25, i++){
+if (i = 1){return 'I am 1 strange loop.'} else {return "I am" +`${i}`+"strange loop"}}
+return array
+
+
+}
+function whileLoop(n){
+  while (n > 0){console.log (--n)};
+console.log('done')
+}
+console.log(whileLoop(3))
