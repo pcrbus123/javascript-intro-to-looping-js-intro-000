7bef8b861081cd533d3d3042256dc5dfdb34340f
Pnina Breslav <pcrbus123@gmail.com>
2018-11-04 05:57:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
 for (var i=0, i< 25, i++){
-if (i = 1){return 'I am 1 strange loop.'} else {return "I am" +`${i}`+"strange loop"}}
+if (i === 1){return 'I am 1 strange loop.'} else {return "I am" +`${i}`+"strange loop"}}
 return array
 
 
